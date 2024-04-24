4c27a12e833a5ffedc0748ac628cb15d652e158e
Pedro Almeida <pedrohas35@gmail.com>
2024-04-24 17:24:14 -0300

    Lógica da UI

--- Script/Controla_Poder.cs
+++ Script/Controla_Poder.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Controla_Poder : MonoBehaviour
 {
     public GameObject Poder ;
-    public GameObject Inimigo;
+    public GameObject Inimigo; 
+    private Collision2D collision2DPoder;
+
+
     void Start()
     {
-        
+        collision2DPoder = Poder.GetComponent<Collision2D>();
+        Poder.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        AcompanhaMouse();
+        //OnCollisionEnter2D(collision2DPoder);
         
     }
+   private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verifica se o collider que colidiu tem a tag "Inimigo"
+        if (collision.gameObject.CompareTag("Inimigo"))
+        {
+            Destroy(collision.gameObject);
+        } 
+        
+
+        }
+private void AcompanhaMouse(){
+         Vector3 mousePosition = Input.mousePosition;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Poder.transform.position = worldPosition;
+    }
 }
+
--- Script/Controla_interface.cs
+++ Script/Controla_interface.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Controla_interface : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Poder ;
+    public UnityEngine.UI.Button botao;
     void Start()
     {
-        
+        Poder.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OnButtonClick(){
+        if(!Poder.activeSelf){
+            Poder.SetActive(true);
+        
+        } 
+        if(Poder.activeSelf){
+            Destroy(botao.gameObject);
+        }
+        
+        
+    }
+    private void  destativabotao( ){
+
     }
 }
